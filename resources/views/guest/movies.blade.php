99e73f74001d955042247280ca68e583b4436f26
isma-91 <isma91@hotmail.it>
2023-01-16 15:59:24 +0100

    Add movie page with styles

--- resources/views/guest/movies.blade.php
+++ resources/views/guest/movies.blade.php
@@ -1,5 +1,14 @@
-@extends('guest.layout.base')
+@extends('guest.layouts.base')
 
 @section('content')
-
+    <ul class="container">
+        @foreach ($movies as $movie)
+            <li class="card">
+                <p><span>Titolo:</span> {{ $movie->title }}</p>
+                <p><span>Titolo Originale:</span> {{ $movie->original_title }}</p>
+                <p><span>Data di uscita:</span> {{ $movie->date }}</p>
+                <p><span>Voto:</span> {{ $movie->vote }}</p>
+            </li>
+        @endforeach
+    </ul>
 @endsection
